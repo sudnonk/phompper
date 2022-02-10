cf1f863d993702f63c65f2d0df8b63ed90806cc9
sudnonk <sudnonk12@yahoo.co.jp>
2022-02-10 14:28:54 +0900

    googlemapsが動作することを確認

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,15 @@
 let map;
 let marker = null;
 
+/**
+ * GoogleMapsを初期化する
+ */
 function initMap() {
+    //デフォルトでは東京駅を表示する
     const defPos = new google.maps.LatLng({lat: 35.681217751538604, lng: 139.76709999359113});
     const mapDOM = document.getElementById("map");
     if (mapDOM === null) {
-        setInfo("エラーが発生しました。管理者に連絡してください。");
+        setMapStatus("エラーが発生しました。管理者に連絡してください。");
         return;
     }
     map = new google.maps.Map(mapDOM, {
@@ -22,10 +26,15 @@ function initMap() {
     map.addListener("center_changed", function () {
         console.log("center_changed");
     });
+    console.log("Google Mapsを読み込みました。")
 }
 
 window.initMap = initMap;
 
+/**
+ * クリックされた場所に地図の中心を移動する
+ * @param mapMouseEvent
+ */
 function moveToPoint(mapMouseEvent) {
     const lat = mapMouseEvent.latLng.lat();
     const lng = mapMouseEvent.latLng.lng();
@@ -38,15 +47,27 @@ function moveToPoint(mapMouseEvent) {
     map.panTo(pos);
 }
 
+/**
+ * mapのposの箇所にマーカーを立てる
+ * @param pos
+ * @param map
+ */
 function reMarker(pos, map) {
+    //現在のマーカーを消す
     if (marker !== null) {
         marker.setMap(null);
     }
     marker = new google.maps.Marker({position: pos, map: map});
+    getAddress(pos);
 }
 
+/**
+ * ブラウザの地理的位置を取得する
+ * @param defaultPosition 取得に失敗した際に表示する場所
+ * @return {*}
+ */
 function getUserLocation(defaultPosition) {
-    setInfo("現在位置を取得しています・・・");
+    setMapStatus("現在位置を取得しています・・・");
     let pos = defaultPosition;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -57,24 +78,51 @@ function getUserLocation(defaultPosition) {
         });
     }
     if (pos === defaultPosition) {
-        setInfo("現在位置の取得に失敗しました。東京を表示します。");
+        setMapStatus("現在位置の取得に失敗しました。東京を表示します。");
         return defaultPosition;
     } else {
         return pos;
     }
 }
 
-function setInfo(str) {
+/**
+ * latlngの場所の住所を取得して表示する
+ * @param latlng
+ */
+function getAddress(latlng) {
+    const geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+        latLng: latlng
+    }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0].geometry) {
+                document.getElementById("address").innerText = results[0].formatted_address.replace(/^日本(、|,)/, '');
+            }
+        } else {
+            document.getElementById("address").innerText = "住所の取得に失敗しました";
+        }
+    });
+}
+
+/**
+ * GoogleMapsの状態を表示する
+ * @param str
+ */
+function setMapStatus(str) {
     console.log(str);
-    const infoDOM = document.getElementById("info");
+    const infoDOM = document.getElementById("map_status");
     if (infoDOM === null) {
         return;
     }
     infoDOM.innerText = str;
 }
 
+/**
+ * GoooleMapsの認証に失敗したときに実行される関数
+ */
 function gm_authFailure() {
-    setInfo("Google Mapsを読み込めませんでした。");
+    setMapStatus("Google Mapsを読み込めませんでした。");
 }
 
 window.gm_authFailure = gm_authFailure;
--- resources/views/maptest.blade.php
+++ resources/views/maptest.blade.php
@@ -5,14 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>テスト</title>
+    <link rel="stylesheet" type="text/css"  href="{{asset('/css/main.css')}}">
 </head>
 <body>
-<p id="info">地図を読み込んでいます・・・</p>
+<p id="map_status">地図を読み込んでいます・・・</p>
 <div id="map"></div>
+<p id="address">住所の読み込みに失敗しました</p>
 </body>
-<script src="{{asset('/js/index.js',true)}}"></script>
+<script src="{{asset('/js/index.js')}}"></script>
 <script
-    src="https://maps.googleapis.com/maps/api/js?key={{config('googlemap.api-key')}}&callback=initMap&libraries=&v=weekly"
+    src="https://maps.googleapis.com/maps/api/js?key={{config('googlemap.api-key')}}&callback=initMap&v=weekly"
     async
 ></script>
 </html>
