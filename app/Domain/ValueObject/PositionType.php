d6d6fce71a57b9ab2a6f337f6c875589c7f26b5d
sudnonk <sudnonk12@yahoo.co.jp>
2022-03-26 00:27:42 +0900

    PositionTypeをEnumのVOにした

---
+++ app/Domain/ValueObject/PositionType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\ValueObject;
+
+class PositionType extends EnumValueObject
+{
+    protected static $name = "地点種別";
+
+    const DENSHIN = "電信柱";
+    const DENCHU = "電柱";
+    const BUILDING = "通信ビル";
+    const OTHER = "その他";
+}
