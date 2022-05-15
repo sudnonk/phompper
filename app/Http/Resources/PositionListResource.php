7ba2c7f74363d803f929aa0da38ef1ec580950ad
sudnonk <sudnonk12@yahoo.co.jp>
2022-05-15 14:50:01 +0900

    地点一覧表示用のResourceを作成

---
+++ app/Http/Resources/PositionListResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Entity\Position\Position;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PositionListResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        /** @var Position[] $positions */
+        $positions = $this->resource;
+
+        $data = [];
+        foreach ($positions as $position) {
+            $datum = [
+                'geoHash'=>$position->geoHash->value,
+                'latitude' => $position->geoHash->latitude,
+                'longitude' => $position->geoHash->longitude,
+                'type' => $position->positionType->value
+            ];
+
+            $data[] = $datum;
+        }
+
+        return $data;
+    }
+}
